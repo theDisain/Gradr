cc37506fedff0ee79d698433f1535e3d3f45ffb5
Ain-Joonas Toose <Ain.Joonas.Toose@gmail.com>
2016-03-17 10:33:16 +0200

    added registering modal and did some css work. Also a navbar.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,7 +3,18 @@ $(document).foundation();
 $('#forgotPassword').click(function () {
     $('#forgotPaswordModal').foundation('open');
 });
-$('#loginForm').submit(function() {
-    window.location.href = "http://stackoverflow.com";
+$('#cancelRegisterBtn').click(function () {
+    $('#registerAccountModal').foundation('close');
+});
+
+$('#loginForm').submit(function (e) {
+    e.preventDefault();
+    document.location = 'main.html';
+    return false;
+});
+$('#registerForm').submit(function (e) {
+    e.preventDefault();
+    $('#registerAccountModal').foundation('close');
+    $('#thankYouForRegisteringModal').foundation('open');
     return false;
 });
